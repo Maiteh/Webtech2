aa4fb8e984cff3df13e5072bbe8cf822b3b1d373
Maiteh <maiteh_m@hotmail.com>
2014-03-21 21:51:40 +0100

    js verder

--- Week4/js/app.js
+++ Week4/js/app.js
@@ -14,3 +14,68 @@ if(navigator.geolocation){
 function error(){
 	alert('No location found!');
 }
+function getPosition(position){
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    codeLatLng(lat, lon);
+    w = new Weather(lat, lon);
+}
+  function codeLatLng(lat, lng) {
+
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+         //formatted address
+         city = results[3].formatted_address;
+        } else {
+          alert("No results found");
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+  }
+//End location
+
+// Change day
+ var days = $("a");
+  days.each(function(day){
+    $(this).on("click", function(){
+     weatherData(day-1);
+    });
+  });
+// Weather
+var theWeather;
+var weatherDay
+var skycons = new Skycons({"color": "white"});
+
+var Weather = function (lat, lon){
+    this.lat = lat;
+    this.lon = lon;
+    this.key = "b90e2409c25c39a24e68b8dd6d219976";
+    check();
+}
+/* Gebruik van localStorage: In het geheugen opslaan van data
+
+    * localStorage.setItem("NAAM", data in die naam)
+    * localStorage.getItem("NAAM")
+
+*/
+function check(){
+    if(!localStorage.getItem("weatherdata")){
+        // Not a localStorage
+        loadData();
+    }else{
+        // There is a storage
+        if(Date().getTime - localStorage.getItem('time')< 300000){
+            console.log("Data up-to-date");
+        }else{
+            // Old data in the storage
+            loadData();
+        }
+    }
+}
+
+
+}
