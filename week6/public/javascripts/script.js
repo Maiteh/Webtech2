79e6cf663c785f7078021c20c37f614be5d5d578
Maiteh <maiteh_m@hotmail.com>
2014-04-04 23:38:18 +0200

    script aangepast

--- week6/public/javascripts/script.js
+++ week6/public/javascripts/script.js
@@ -5,3 +5,5 @@ client.subscribe('/ask', function(message)
 	$('#questions ul').append('<li><span>' + message.text[0] + '</span> asked a question: <blockquote>' + message.text[1] + '</blockquote></li>');
 });
 
+$('#send').click(sendMessage);
+
