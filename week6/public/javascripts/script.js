dcce099bf71234c85cd33c2fc582058ef2a20e43
Maiteh <maiteh_m@hotmail.com>
2014-04-04 23:38:06 +0200

    js aangepast

--- week6/public/javascripts/script.js
+++ week6/public/javascripts/script.js
@@ -1,2 +1,7 @@
 var client = new Faye.Client('/faye');
 
+client.subscribe('/ask', function(message) 
+{
+	$('#questions ul').append('<li><span>' + message.text[0] + '</span> asked a question: <blockquote>' + message.text[1] + '</blockquote></li>');
+});
+
